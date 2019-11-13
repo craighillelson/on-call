01956cd163d0e1702cf53c9287f857489711d7d0
Craig Hillelson <craighillelson@gmail.com>
2019-11-13 13:31:44 -0500

    updated on-call.py

--- on-call.py
+++ on-call.py
@@ -1,8 +1,21 @@
 """ __doc__ """
 
-import functions
 import imports
 import employees
+
+def print_employee_holiday_week(a, b, c, d):
+    if a < date < b or c < date < d:
+        print(f"{date.strftime('%Y-%m-%d')} - {employee_assigned}"
+        " - holiday week")
+    else:
+        print(f"{date.strftime('%Y-%m-%d')} - {employee_assigned}")
+
+
+def format_dates(x):
+    x = str(year) + '-' + x
+    x = imports.datetime.strptime(x, '%Y-%m-%d')
+    return x
+
 
 D = imports.datetime.now()
 year = D.year
@@ -12,17 +25,17 @@ tgiving_wk_end = '11-29'
 xmas_wk_start = '12-18'
 xmas_wk_end = '12-26'
 
-tgiving_wk_start = functions.format_dates(tgiving_wk_start)
-tgiving_wk_end = functions.format_dates(tgiving_wk_end)
-xmas_wk_start = functions.format_dates(xmas_wk_start)
-xmas_wk_end = functions.format_dates(xmas_wk_end)
+tgiving_wk_start = format_dates(tgiving_wk_start)
+tgiving_wk_end = format_dates(tgiving_wk_end)
+xmas_wk_start = format_dates(xmas_wk_start)
+xmas_wk_end = format_dates(xmas_wk_end)
 
 MONDAYS = []
 EMPLOYEE_ASSIGNMENTS = []
 
 number_of_weeks = input("How many weeks in the future would you like to make \
 the schedule for? ")
-number_of_weeks = number_of_weeks + 1
+number_of_weeks = int(number_of_weeks) + 1
 
 for i in range(1, number_of_weeks, 1):
     MONDAYS.append(D + imports.relativedelta(weekday=imports.MO(+i)))
@@ -36,5 +49,5 @@ for n, employee in enumerate(imports.cycle(employees.EMPLOYEES)):
 ASSIGNMENTS = dict(zip(MONDAYS, EMPLOYEE_ASSIGNMENTS))
 
 for date, employee_assigned in sorted(ASSIGNMENTS.items()):
-    functions.print_employee_holiday_week(tgiving_wk_start, tgiving_wk_end, \
+    print_employee_holiday_week(tgiving_wk_start, tgiving_wk_end, \
     xmas_wk_start, xmas_wk_end)
