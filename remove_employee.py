3b537012299bfe16bcf38f349241b4d968e109a7
craighillelson <craighillelson@gmail.com>
2019-11-30 09:54:38 -0500

    updated remove_employee.py

--- remove_employee.py
+++ remove_employee.py
@@ -1,11 +1,27 @@
 import csv
+from collections import namedtuple
 
 lines = list()
+RTN = lambda: '\n'
+
+dct = {}
+
+with open('employees.csv') as csv_file:
+    F_CSV = csv.reader(csv_file)
+    COLUMN_HEADINGS = next(F_CSV)
+    CSV_ROW = namedtuple('Row', COLUMN_HEADINGS)
+    for rows in F_CSV:
+        row = CSV_ROW(*rows)
+        print(f'{row.employee} {row.start_date}')
+
 mem = input('Please enter a member\'s name to be deleted. \n')
 
-with open('employees.csv', 'r') as readFile:
-    reader = csv.reader(readFile)
-    for row in reader:
+with open('employees.csv') as csv_file:
+    F_CSV = csv.reader(csv_file)
+    COLUMN_HEADINGS = next(F_CSV)
+    CSV_ROW = namedtuple('Row', COLUMN_HEADINGS)
+    for rows in F_CSV:
+        row = CSV_ROW(*rows)
         lines.append(row)
         for field in row:
             if field == mem:
@@ -15,4 +31,8 @@ with open('employees.csv', 'w') as writeFile:
     writer = csv.writer(writeFile)
     writer.writerows(lines)
 
+print(RTN())
+
 print(f'{mem} deleted successfully')
+
+print(RTN())
