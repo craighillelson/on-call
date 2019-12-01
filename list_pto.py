0a72f262aaaa83b189f5e64102094c025211e018
craighillelson <craighillelson@gmail.com>
2019-12-01 11:27:33 -0500

    updated list_pto.py

--- list_pto.py
+++ list_pto.py
@@ -6,8 +6,8 @@ from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
 from dateutil.rrule import MO
 
-def output_date(a):
-    """ format date """
+def format_date(a, b):
+    a = datetime.strptime(b, '%Y-%m-%d')
     return a.strftime('%Y-%m-%d')
 
 
@@ -15,23 +15,25 @@ RTN = lambda: "\n"
 
 PTO = {}
 
-print(RTN())
-
 with open('pto.csv') as csv_file:
     F_CSV = csv.reader(csv_file)
     COLUMN_HEADINGS = next(F_CSV)
     CSV_ROW = namedtuple('Row', COLUMN_HEADINGS)
     for rows in F_CSV:
         row = CSV_ROW(*rows)
+        pto_start = format_date('end_date_strptime', row.end_date)
+        pto_end = format_date('end_date_strptime', row.end_date)
+        PTO[row.employee] = [pto_start, pto_end]
+        # start_date_strptime = format_date('start_date_strptime', row.start_date)
+        # pto_start = output_date(start_date_strptime)
+        # end_date_strptime = format_date('end_date_strptime', row.end_date)
+        # pto_end = output_date(end_date_strptime)
+        # start_date_strptime = datetime.strptime(row.start_date, '%Y-%m-%d')
+        # end_date_strptime = datetime.strptime(row.end_date, '%Y-%m-%d')
         # start_date_strptime = datetime.strptime(row.start_date, '%Y-%m-%d')
         # end_date_strptime = datetime.strptime(row.end_date, '%Y-%m-%d')
         # pto_start = start_date_strptime + relativedelta(weekday=MO(-1))
         # pto_end = end_date_strptime + relativedelta(weekday=MO(+2))
-        start_date_strptime = datetime.strptime(row.start_date, '%Y-%m-%d')
-        pto_start = output_date(start_date_strptime)
-        end_date_strptime = datetime.strptime(row.end_date, '%Y-%m-%d')
-        pto_end = output_date(end_date_strptime)
-        PTO[row.employee] = [pto_start, pto_end]
         # print(row.employee)
         # print(f'{pto_start} - {pto_end}')
         # print(RTN())
@@ -47,4 +49,5 @@ with open('pto.csv') as csv_file:
     # print('-' * 40)
     # print(RTN())
 
-# print(RTN())
+# for k, v in PTO.items(): # rename dct, keys, and values to make them meaningful
+    # print(k, v[0], v[1]) # process row
