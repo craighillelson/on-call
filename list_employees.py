0373d2df46c3a920db824fbe05c58f48638bccee
craighillelson <craighillelson@gmail.com>
2019-12-02 07:46:18 -0500

    first commit

---
+++ list_employees.py
@@ -0,0 +1,11 @@
+import employees
+
+RTN = lambda: "\n"
+
+print(RTN())
+
+print('name, first eligible shift')
+for emp, first_eligible_shift in employees.EMPLOYEES_FIRST_ELIGIBLE.items():
+    print(f'{emp}, {first_eligible_shift}')
+
+print(RTN())
