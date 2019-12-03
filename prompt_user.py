f3bbdffb82f57160d063f3b85810a517927aeb7b
craighillelson <craighillelson@gmail.com>
2019-12-03 07:22:37 -0500

    linted prompt_user.py

--- prompt_user.py
+++ prompt_user.py
@@ -1,13 +1,15 @@
-RTN = lambda: "\n"
+""" __doc__ """
+
+RTN = lambda: '\n'
 print(RTN())
 
 # prompt user for start date
 # validate user input
-start_today = input('Would you like to create a schedule that starts on the '
-'next Monday? (Y/N) ')
+START_TODAY = input('Would you like to create a schedule that starts on the '
+                    'next Monday? (Y/N) ')
 
-if start_today == 'Y':
-    num = input('How many weeks into the future would you like to make the '
-    'schedule for? ')
+if START_TODAY == 'Y':
+    NUM = input('How many weeks into the future would you like to make the '
+                'schedule for? ')
 else:
     pass
