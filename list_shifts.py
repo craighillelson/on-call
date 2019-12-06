21c591740c9603c1021d26e50ed93b20112a6e22
Craig Hillelson <craighillelson@gmail.com>
2019-12-06 11:24:55 -0500

    updated list_shifts.py

--- list_shifts.py
+++ list_shifts.py
@@ -1,1 +1,26 @@
-# list shifts by employee
+""" __doc__ """
+
+import csv
+from collections import namedtuple
+
+RTN = lambda: "\n"
+
+dct = {}
+
+with open('shifts.csv') as csv_file:
+    F_CSV = csv.reader(csv_file)
+    COLUMN_HEADINGS = next(F_CSV)
+    CSV_ROW = namedtuple('Row', COLUMN_HEADINGS)
+    for rows in F_CSV:
+        row = CSV_ROW(*rows)
+        dct[row.shift] = row.employee
+
+print(RTN())
+
+# do holiday week calculations here
+
+print('shift, employee')
+for shift, emp in dct.items():
+    print(shift, emp)
+
+print(RTN())
