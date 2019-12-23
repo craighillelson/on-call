a26e436a8c954705020348829d3232a39d64a381
Craig Hillelson <craighillelson@gmail.com>
2019-12-22 21:06:27 -0500

    added start and end of quarter switch statement

--- prompt_user.py
+++ prompt_user.py
@@ -1,6 +1,18 @@
 """ __doc__ """
 
+def num_to_month_day(argument):
+    """ switch case statement """
+    switcher = {
+        '1': ['01-01', '03-31',],
+        '2': ['04-01', '06-30',],
+        '3': ['07-01', '09-30',],
+        '4': ['10-01', '12-31',],
+    }
+    return switcher.get(argument, 'nothing')
+
+
 RTN = lambda: '\n'
+
 print(RTN())
 
 # prompt user for start date
@@ -15,3 +27,14 @@ print('By defualt, schedules created to start on the next Monday and '
 
 NUM = 12
 # schedules should be made for an entire quarter at a time
+
+# qtr = input('What quarter would you like the schedule to start? '
+            '(enter a number between 1 and 4\n')
+# year = input('In what year (enter in YYYY format)\n')
+
+# month_day = num_to_month_day(qtr)
+# start = month_day[0]
+# end = month_day[1]
+# qtr_start = year + '-' + start
+# qtr_end = year + '-' + end
+# print(f'Quarter {qtr} - {qtr_start} - {qtr_end}')
