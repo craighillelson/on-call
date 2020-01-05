bef667330fd6de25a6d682af19d51baf9b59f060
craighillelson <craighillelson@gmail.com>
2020-01-05 07:54:16 -0500

    updated create_schedule

--- create_schedule.py
+++ create_schedule.py
@@ -36,6 +36,16 @@ for shift, employee in ASSIGNMENTS.items():
     print(shift, employee)
 
 # write to csv
+HEADERS = 'shift', 'employee'
+
+with open('shifts.csv', 'w') as out_file:
+    out_csv = csv.writer(out_file)
+    out_csv.writerow(HEADERS)
+    for shift, employee in ASSIGNMENTS.items():
+        keys_values = (shift, employee)
+        out_csv.writerow(keys_values)
+
+print('"shifts.csv" exported successfully')
 
 print(RTN())
 
