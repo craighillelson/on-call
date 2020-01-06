55fd78f5c291614f85b1d7903aeac0643cccd343
craighillelson <craighillelson@gmail.com>
2020-01-06 07:22:48 -0500

    created eligible_employees.py

---
+++ eligible_employees.py
@@ -0,0 +1,14 @@
+""" __doc__ """
+
+with open('shifts.txt', 'r') as txt_file:
+    shifts = [shift.rstrip() for shift in txt_file.readlines()]
+
+# RTN = lambda: '\n'
+
+# print(RTN())
+
+# print('shifts')
+# for shift in shifts:
+    # print(shift)
+
+# print(RTN())
