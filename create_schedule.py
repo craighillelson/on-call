77989f4afb5438337b4f94997d94bf087beaa446
craighillelson <craighillelson@gmail.com>
2020-01-08 07:01:10 -0500

    updated create_schedule.py

--- create_schedule.py
+++ create_schedule.py
@@ -1,87 +1,127 @@
 """ __doc__ """
 
-import csv
-import employees
-import shifts
+# imports
 from datetime import date
+from datetime import datetime
+from collections import namedtuple
 from itertools import cycle
+import csv
+from dateutil.relativedelta import relativedelta
+from dateutil.rrule import MO
+import prompt_user
 
+# variables
 RTN = lambda: '\n'
+EMP_START_DATES = []
 ELIGIBLE_EMPLOYEES = []
-ASSIGNMENTS = {}
+EMPLOYEES = []
+INDEXES = []
+ASSIGNMENTS = []
+QTR_START_END_DATES = []
+SHIFTS = []
+YEAR = str(prompt_user.YEAR)
 
-print('shift start dates')
-for monday in shifts.SHIFTS:
-    print(monday)
+def start_end_date_str(argument):
+    """ switch case statement """
+    switcher = {
+        1: ['01-01', '03-31'],
+        2: ['04-01', '06-30'],
+        3: ['07-01', '09-30'],
+        4: ['10-01', '12-31'],
+        }
+    return switcher.get(argument, 'nothing')
+
+
+def cat_date(a_a):
+    """ concatenates year with start and end dates for each quarter """
+    date_ymd = YEAR + '-' + a_a
+    return date_ymd
+
+
+def append_list():
+    """ appends var to list """
+    INDEXES.append(i)
+
+QTR_START_END = start_end_date_str(prompt_user.starting_qtr)
+
+# append list QTR_START_END_DATES
+for qtr_date in QTR_START_END:
+    date_str = cat_date(qtr_date)
+    date_fmt = datetime.strptime(date_str, '%Y-%m-%d')
+    date = date_fmt.date()
+    QTR_START_END_DATES.append(date)
+
+# find first and last Mondays of the quarter
+QTR_START_DATE = QTR_START_END_DATES[0]
+QTR_END_DATE = QTR_START_END_DATES[1]
+
+# determine number of weeks between the start and end of the quarter
+DATE_DELTA = QTR_END_DATE - QTR_START_DATE
+WEEKS_BETWEEN = round(DATE_DELTA.days / 7)
+
+# append SHIFTS with the date of each Monday in the selected quarter
+for i in range(1, WEEKS_BETWEEN + 1, 1):
+    SHIFTS.append(QTR_START_DATE + relativedelta(weekday=MO(+i)))
+
+# write shifts to a text file
+with open('shifts.txt', 'w') as out_file:
+    OUT_CSV = csv.writer(out_file)
+    for shift in SHIFTS:
+        shift_str = str(shift)
+        OUT_CSV.writerow([shift_str])
+
+# import 'employees.csv' and populate two lists: EMPLOYEES and EMP_START_DATES
+with open('employees.csv', 'r') as csv_file:
+    F_CSV = csv.reader(csv_file)
+    COLUMN_HEADINGS = next(F_CSV)
+    CSV_ROW = namedtuple('Row', COLUMN_HEADINGS)
+    for rows in F_CSV:
+        row = CSV_ROW(*rows)
+        emp = row.employee
+        start = row.start_date
+        EMPLOYEES.append(emp)
+        EMP_START_DATES.append(start)
+
+i = 0
+
+# based on employee start dates, populate a list of employees eligible to work
+# on call shifts
+for shift, start_date in zip(SHIFTS, cycle(EMP_START_DATES)):
+    start_date_fmt = datetime.strptime(start_date, '%Y-%m-%d').date()
+    first_eligible = start_date_fmt + relativedelta(weekday=MO(+12))
+    if shift > first_eligible:
+        if i >= len(EMP_START_DATES):
+            i = 0
+            append_list()
+        else:
+            append_list()
+    else:
+        i = 0
+        append_list()
+    i += 1
+
+for ind, emp in zip(INDEXES, cycle(EMPLOYEES)):
+    ASSIGNMENTS.append(EMPLOYEES[ind])
 
 print(RTN())
 
-print('eligible employees')
-for employee, date_first_eligible in employees.EMPLOYEES_FIRST_ELIGIBLE.items():
-    print(employee)
-    ELIGIBLE_EMPLOYEES.append(employee)
-
-# loop through shifts, if employee is not on pto, assign a shift
-# if employee is on pto, skip to next employee
-
-for shift, emp in zip((shifts.SHIFTS), cycle(ELIGIBLE_EMPLOYEES)):
-    ASSIGNMENTS[shift] = emp
+# output assignments
+print('assignments')
+for shift, assignment in zip(SHIFTS, ASSIGNMENTS):
+    print(shift, assignment)
 
 print(RTN())
 
-print('output schedule')
-for shift, employee in ASSIGNMENTS.items():
-    print(shift, employee)
-
-# write to csv
+# write assignments to csv
 HEADERS = 'shift', 'employee'
 
 with open('assignments.csv', 'w') as out_file:
-    out_csv = csv.writer(out_file)
-    out_csv.writerow(HEADERS)
-    for shift, employee in ASSIGNMENTS.items():
-        keys_values = (shift, employee)
-        out_csv.writerow(keys_values)
+    OUT_CSV = csv.writer(out_file)
+    OUT_CSV.writerow(HEADERS)
+    for shift, assignment in zip(SHIFTS, ASSIGNMENTS):
+        OUT_CSV.writerow([shift, assignment])
+
+# alert user
+print('"assignments.csv" was exported successfully')
 
 print(RTN())
-
-print('"assignments.csv" exported successfully')
-
-print(RTN())
-
-# i = 1
-# TODAY = date.today()
-# ELIGIBLE_EMPLOYEES = []
-# ASSIGNMENTS = {}
-
-# for emp in employees.EMPLOYEES_LST:
-#     if i >= shifts.num_weeks:
-#         break
-#     name = emp[0]
-#     first_eligible = emp[1]
-#     if first_eligible < TODAY:
-#         ELIGIBLE_EMPLOYEES.append(emp[0])
-#     else:
-#         pass
-#     i += 1
-
-# i = 1
-
-# print('shift, employee')
-# for shift, emp in ASSIGNMENTS.items():
-#     print(f'{i} - {shift} - {emp}')
-#     i += 1
-
-# print(RTN())
-
-# with open('shifts.csv', 'w') as out_file:
-#     HEADERS = 'shift', 'employee'
-#     out_csv = csv.writer(out_file)
-#     out_csv.writerow(HEADERS)
-#     for shift, emp in ASSIGNMENTS.items():
-#         keys_values = (shift, emp)
-#         out_csv.writerow(keys_values)
-
-# print('"shifts.csv" exported successfully')
-
-# print(RTN())
