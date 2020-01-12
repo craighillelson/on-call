1c904840af1056dd93df18e1eb7c35a0c7f617ed
craighillelson <craighillelson@gmail.com>
2020-01-12 00:20:02 -0500

    updated pto.py

--- pto.py
+++ pto.py
@@ -2,28 +2,35 @@
 
 import csv
 import datetime
+import functions
 from collections import namedtuple
 from datetime import datetime
+from dateutil.relativedelta import relativedelta
+from dateutil.rrule import MO
 
 RTN = lambda: '\n'
 
 def format_date(a, b):
-    """ format date """
-    a = datetime.strptime(b, '%Y-%m-%d')
-    return a.strftime('%Y-%m-%d')
+    a = row.start_date
+    b = datetime.strptime(a, '%Y-%m-%d').date()
+    return b
 
 
-def date_day(a, b, c, d):
-    """ determine the days of the week that employee's pto starts and ends """
-    a = datetime.strptime(pto[b], '%Y-%m-%d')
-    c = a.date()
-    d = WEEKDAYS[c.weekday()]
-    return d
+def output_start_dates(a, b):
+    """ output start dates """
+    print(f'pto starts: {a}')
+    b = a
+    print(f'unavailable starting {b}')
+
+
+def output_return_dates(a, b, c):
+    """ outputs end dates """
+    print(f'pto ends - {a}')
+    b = a + relativedelta(weekday=MO(+c))
+    print(f'available starting - {b}')
 
 
 PTO = {}
-PTO_START = {}
-PTO_END = {}
 WEEKDAYS = (
     'Monday',
     'Tuesday',
@@ -43,19 +50,25 @@ with open('pto.csv', 'r') as csv_file:
     for rows in F_CSV:
         row = CSV_ROW(*rows)
         emp = row.employee
-        pto_start = format_date('end_date_strptime', row.start_date)
-        pto_end = format_date('end_date_strptime', row.end_date)
-        PTO[emp] = [pto_start, pto_end]
-        PTO_START[pto_start] = emp
-        PTO_END[pto_end] = emp
+        pto_start_date_fmt = format_date('pto_start', 'pto_start_date')
+        pto_end_date_fmt = format_date('pto_end', 'pto_end-date')
+        PTO[emp] = [pto_start_date_fmt, pto_end_date_fmt]
+
 
 print(RTN())
 
 print('pto by employee')
 for emp, pto in PTO.items():
-    start_pto = date_day('pto_start', 0, 'pto_start_date', 'pto_start_day')
-    end_pto = date_day('pto_end', 1, 'pto_end_date', 'pto_end_day')
-    print(f'{emp}')
-    print(f'start: {start_pto} {pto[0]}')
-    print(f'end: {end_pto} {pto[1]}')
+    print(emp)
+    pto_start_date = pto[0]
+    pto_end_date = pto[1]
+    if WEEKDAYS[pto_start_date_fmt.weekday()] == 'Monday':
+        output_start_dates(pto_start_date_fmt, 'unavailable_start')
+    else:
+        output_start_dates(pto_start_date_fmt, 'unavailable_start')
+    print(RTN())
+    if WEEKDAYS[pto_end_date_fmt.weekday()] == 'Monday':
+        output_return_dates(pto_start_date_fmt, 'unavailable_start', 2)
+    else:
+        output_return_dates(pto_start_date_fmt, 'unavailable_start', 1)
     print(RTN())
