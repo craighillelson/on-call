135f6c6194bb65fa94c5aa9013eb51d1347dc2dd
craighillelson <craighillelson@gmail.com>
2020-01-15 08:29:02 -0500

    updated list_pto.py

--- list_pto.py
+++ list_pto.py
@@ -3,52 +3,41 @@
 import csv
 from collections import namedtuple
 from datetime import datetime
-from datetime import timedelta
-from dateutil.relativedelta import relativedelta
-from dateutil.rrule import MO
+# from datetime import timedelta
+# from dateutil.relativedelta import relativedelta
+# from dateutil.rrule import MO
 
 def format_date(a, b):
+    """ format date """
     a = datetime.strptime(b, '%Y-%m-%d')
     return a.strftime('%Y-%m-%d')
 
 
-RTN = lambda: "\n"
+RTN = lambda: '\n'
 
 PTO = {}
 
-with open('pto.csv') as csv_file:
+# populate the dictionary PTO with the contents of 'pto.csv'
+with open('pto.csv', 'r') as csv_file:
     F_CSV = csv.reader(csv_file)
     COLUMN_HEADINGS = next(F_CSV)
     CSV_ROW = namedtuple('Row', COLUMN_HEADINGS)
     for rows in F_CSV:
         row = CSV_ROW(*rows)
-        pto_start = format_date('end_date_strptime', row.end_date)
+        employee = row.employee
+        pto_start = format_date('end_date_strptime', row.start_date)
         pto_end = format_date('end_date_strptime', row.end_date)
-        PTO[row.employee] = [pto_start, pto_end]
-        # start_date_strptime = format_date('start_date_strptime', row.start_date)
-        # pto_start = output_date(start_date_strptime)
-        # end_date_strptime = format_date('end_date_strptime', row.end_date)
-        # pto_end = output_date(end_date_strptime)
-        # start_date_strptime = datetime.strptime(row.start_date, '%Y-%m-%d')
-        # end_date_strptime = datetime.strptime(row.end_date, '%Y-%m-%d')
-        # start_date_strptime = datetime.strptime(row.start_date, '%Y-%m-%d')
-        # end_date_strptime = datetime.strptime(row.end_date, '%Y-%m-%d')
-        # pto_start = start_date_strptime + relativedelta(weekday=MO(-1))
-        # pto_end = end_date_strptime + relativedelta(weekday=MO(+2))
-        # print(row.employee)
-        # print(f'{pto_start} - {pto_end}')
-        # print(RTN())
+        PTO[employee] = [pto_start, pto_end]
 
-# for emp, pto in pto.items():
-    # print(f'employee: {emp}')
-    # print(f'pto starting: {output_date(start_date_strptime)}')
-    # print(f'pto ending: {output_date(end_date_strptime)}')
-    # print(RTN())
-    # print(f"on pto for shift starting: {output_date(pto[0])}")
-    # print(f"available again: {output_date(pto[1])}")
-    # print(RTN())
-    # print('-' * 40)
-    # print(RTN())
+print(RTN())
 
-# for k, v in PTO.items(): # rename dct, keys, and values to make them meaningful
-    # print(k, v[0], v[1]) # process row
+# output employees and pto start and end dates
+# print('employees and pto')
+
+# print(RTN())
+
+# for emp, pto_date_range in PTO.items():
+#     print(f'employee: {emp}')
+#     print(f'pto start date: {pto_date_range[0]}')
+#     print(f'pto end date: {pto_date_range[1]}')
+#     print(RTN())
