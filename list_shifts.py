90ba5accf9a21a07f4c10621f783d99d2d407731
craighillelson <craighillelson@gmail.com>
2020-01-23 07:33:17 -0500

    updated list_shifts.py

--- list_shifts.py
+++ list_shifts.py
@@ -7,28 +7,11 @@ RTN = lambda: "\n"
 
 SCHED = {}
 
+# output shifts
 with open('shifts.csv') as csv_file:
     F_CSV = csv.reader(csv_file)
     COLUMN_HEADINGS = next(F_CSV)
     CSV_ROW = namedtuple('Row', COLUMN_HEADINGS)
     for rows in F_CSV:
         row = CSV_ROW(*rows)
-        SCHED[row.shift] = row.employee
-
-print(RTN())
-
-SCHED_GROUPED_BY_EMP = {}
-
-for shift, emp in sorted(SCHED.items()):
-    SCHED_GROUPED_BY_EMP.setdefault(emp, []).append(shift)
-
-print('shifts grouped by employee')
-print(RTN())
-
-for emp, shifts in SCHED_GROUPED_BY_EMP.items():
-    print(emp)
-    for i, shift in enumerate(shifts, 1):
-        print(i, shift)
-    print(RTN())
-
-# write shifts per employee to individual csvs
+        print(row.shifts)
