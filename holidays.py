ba9ead4060e6a1f2a93178717d34548d8f096a35
Craig Hillelson <craighillelson@gmail.com>
2020-01-23 14:23:08 -0500

    uploaded holidays.py

---
+++ holidays.py
@@ -0,0 +1,15 @@
+TGIVING_WK_START = '11-22'
+TGIVING_WK_END = '11-29'
+XMAS_WK_START = '12-18'
+XMAS_WK_END = '12-26'
+
+# or
+
+holidays = {
+    TGIVING_WK_START: '11-22',
+    TGIVING_WK_END: '11-29',
+    XMAS_WK_START: '12-18',
+    XMAS_WK_END: '12-26',
+}
+
+# regex to evaluate if last four characters contain the above
