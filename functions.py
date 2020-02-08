c90fc678bf7913929ec94f73e509ecda9f24c3d1
craighillelson <craighillelson@gmail.com>
2020-02-08 00:58:58 -0500

    updated functions.py
    ordered functions

--- functions.py
+++ functions.py
@@ -1,5 +1,45 @@
 """ __doc__ """
 
+import csv
+from collections import namedtuple
+import calendar
+import datetime
+
+RTN = lambda: '\n'
+
+def calc_dates(a, b, c):
+    """ calculate dates """
+    a = b + datetime.timedelta(days=-b.weekday(), weeks=c)
+    return a
+
+def find_day(a):
+    """ determine if the start """
+    usr_start_day = datetime.datetime.strptime(a, '%Y-%m-%d').weekday()
+    return (calendar.day_name[usr_start_day])
+
+
+def append_shifts(a, b):
+    """ add dates of the next 13 mondays to the shifts list """
+    for i in range(0, 14, 1):
+        monday = a + datetime.timedelta(days=-a.weekday(), weeks=i)
+        b.append(monday)
+
+
+def fmt_date(a, b):
+    """ format date """
+    a = datetime.datetime.strptime(b, '%Y-%m-%d').date()
+    return a
+
+def open_csv(a):
+    with open('pto.csv') as csv_file:
+        F_CSV = csv.reader(csv_file)
+        COLUMN_HEADINGS = next(F_CSV)
+        CSV_ROW = namedtuple('Row', COLUMN_HEADINGS)
+        for rows in F_CSV:
+            row = CSV_ROW(*rows)
+            a[row.shift] = [row.email]
+
+
 def write_dct_to_csv(a, b, c, d):
     """ write dictionary to a csv """
     import csv
