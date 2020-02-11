d4ebaa92f3998eba0c85a083fd09ca81bd52cef1
craighillelson <craighillelson@gmail.com>
2020-02-11 15:06:40 -0500

    update list_employees and menu

--- list_employees.py
+++ list_employees.py
@@ -5,7 +5,10 @@ import functions
 
 print(functions.RTN())
 
-for i, email in enumerate(employees.EMPLOYEES, 1):
-    print(i, email)
+if employees.EMPLOYEES:
+    for i, email in enumerate(employees.EMPLOYEES, 1):
+        print(i, email)
+else:
+    print('"employees.csv" is empty')
 
 print(functions.RTN())
--- menu.py
+++ menu.py
@@ -10,17 +10,20 @@ while True:
     print('please make a selection')
     print('a - add employees')
     print('b - create schedule')
-    print('c - list pto')
-    print('d - remove employee')
-    print('e - quit')
+    print('c - list employees')
+    print('d - list pto')
+    print('e - remove employee')
+    print('f - quit')
     user_choice = input()
     if user_choice == 'a':
         open_file('add_employees.py')
     elif user_choice == 'b':
         open_file('create_schedule.py')
     elif user_choice == 'c':
-        open_file('list_pto.py')
+        open_file('list_employees.py')
     elif user_choice == 'd':
+        open_file('list_pto.py')
+    elif user_choice == 'e':
         open_file('remove_employee.py')
     else:
         break
