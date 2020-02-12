fedb802e28db982cbbaf5620e502f0fb32b8b254
craighillelson <craighillelson@gmail.com>
2020-02-12 13:43:01 -0500

    update build_sched.py
    added comments

--- build_sched.py
+++ build_sched.py
@@ -25,15 +25,19 @@ today = date.today()
 
 last_shift = create_shifts.SHIFTS[-1]
 
+# populate EMPLOYEES_DCT with employees whose start date is twelve or more weeks
+# in the past
 for email, start_date in employees.EMPLOYEES_DCT.items():
     start_date_fmt = functions.fmt_date('start_date', start_date)
     first_elig_shift = functions.calc_dates('first_elig', start_date_fmt, 12)
     if first_elig_shift < last_shift:
         ELIG_EMPS.append(email)
 
+# zip SHIFTS and ELIG_EMPS to create UNFILTERED_ASSIGNMENTS
 for shift, email in zip(create_shifts.SHIFTS, cycle(ELIG_EMPS)):
     UNFILTERED_ASSIGNMENTS[shift] = email
 
+# find pto conflicts
 for i, ((k, v), (k2, v2)) in enumerate(zip(UNFILTERED_ASSIGNMENTS.items(),
                                            pto.PTO.items()), 1):
     pto_shift = functions.fmt_date('pto_shift', k2)
@@ -48,6 +52,7 @@ for i, ((k, v), (k2, v2)) in enumerate(zip(UNFILTERED_ASSIGNMENTS.items(),
 
 remainder = 13 - i
 
+# output shifts and assignments
 if remainder == 0:
     print('schedule - no conflicts')
     for i, (shift, email) in enumerate(ASSIGNMENTS.items(), 1):
@@ -68,6 +73,7 @@ else:
         else:
             pass
 
+    # iterate through employees for the number of shifts not yet assigned
     print('pick up from resolution')
     for i, email in enumerate(EMPS_RES, 1):
         print(i, email)
@@ -86,6 +92,7 @@ else:
 
     print(functions.RTN())
 
+    # output assignments through resolved conflict
     print('assignments ending in resolution')
     for i, (shift, email) in enumerate(ASSIGNMENTS.items(), 1):
         print(i, shift, email)
@@ -95,24 +102,19 @@ else:
     for shift, email in zip(REMAINING_SHIFTS, REMAINING_EMPS):
         RESOLVED_ASSIGNMENTS[shift] = email
 
-    # concat dictionaries
+    # concatenate dictionaries
     SCHED = dict(ASSIGNMENTS)
     SCHED.update(RESOLVED_ASSIGNMENTS)
 
+    # output resolved assignments
     print('resolved assignments')
     for i, (shift, email) in enumerate(SCHED.items(), 1):
         print(i, shift, email)
 
+# write SCHED to csv
 file_name = str(today) + '_assignments.csv'
 
-HEADERS = 'shift','email'
-
-with open(file_name, 'w') as out_file:
-    out_csv = csv.writer(out_file)
-    out_csv.writerow(HEADERS)
-    for shift, email in SCHED.items():
-        keys_values = (shift, email)
-        out_csv.writerow(keys_values)
+functions.write_dct_to_csv(['shift','email'], file_name, 'shift, email', SCHED)
 
 # update user
 print(functions.RTN())
