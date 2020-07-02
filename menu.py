b7cd48809a6c158509a373f6ffdd6456a37b6335
Craig Hillelson <craighillelson@gmail.com>
2020-07-02 05:33:43 -0400

    update menu.py
    added second return preceding function definition
    edited doc strings
    removed unnecessary comments

--- menu.py
+++ menu.py
@@ -14,25 +14,25 @@ USER_OPTIONS = {
 
 KEYS = list(USER_OPTIONS.keys())
 
+
 def switch_case(argument):
-    """ switch case statement """
+    """Switch case statement."""
     USER_OPTIONS
     return USER_OPTIONS.get(argument, 'nothing')
 
 
 def open_file(a):
-    """ based on user choice, open file """
+    """Based on user choice, open file."""
     exec(open(a).read())
 
 
-# prompt user to select one of the options provided
 print(functions.RTN())
 print('please select an option or press return to quit')
 for opt, action in USER_OPTIONS.items():
     print(f'{opt} - {action}')
 
 while True:
-    usr_choice = input()
+    usr_choice = input('\n> ')
     if usr_choice in KEYS:
         open_file(switch_case(usr_choice))
         break
