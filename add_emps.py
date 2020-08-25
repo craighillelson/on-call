eb8b6bb6188faae295862c3ce726bc58bdcec510
craighillelson <craighillelson@gmail.com>
2020-08-25 08:54:34 -0400

    update add_emps.py
    linted code, added doc string

--- add_emps.py
+++ add_emps.py
@@ -1,30 +1,25 @@
-""" __doc__ """
+"""Add employees to emps.csv."""
 
-# imports
-import csv
+# import csv
 import functions
 import emps
 
-# prompt user
-# get domain name from existing employees
-domain = input('What is your domain name?\n')
+domain = input("What is your domain name?\n")
 
 while True:
-    print('Enter the employee\'s name (or enter nothing to stop.):')
+    print("Enter the employee's name (or enter nothing to stop.):")
     email_prefix = input()
-    email = email_prefix + '@' + domain
+    email = email_prefix + "@" + domain
     if email_prefix == '':
         break
-    print('start date')
+    print("start date")
     start_date = input()
     emps.EMPLOYEES_DCT[email] = start_date
 
-# write dictionary to csv
-functions.csv_write(['email','start_date'], 'emps.csv',
-                           'email, start_date', emps.EMPLOYEES_DCT)
+functions.csv_write(["email", "start_date"], "emps.csv",
+                    "email, start_date", emps.EMPLOYEES_DCT)
 
-# update user
-print('updated list of employees')
-print('email, start date')
+print("updated list of employees")
+print("email, start date")
 for email, start_date in emps.EMPLOYEES_DCT.items():
     print(email, start_date)
