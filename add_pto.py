80b8361c8e2d1d420674631b3a4bb06df6c1ed0e
craighillelson <craighillelson@gmail.com>
2020-08-28 00:41:54 -0400

    update add_pto.py
    linted code
    included inline returns

--- add_pto.py
+++ add_pto.py
@@ -1,4 +1,4 @@
-"""Add PTO to 'pto.csv'.""" 
+"""Add PTO to 'pto.csv'."""
 
 import csv
 from collections import namedtuple
@@ -11,14 +11,14 @@ EMP_DCT = {}
 PTO = {}
 NUMS = []
 
-print(functions.RTN())
+# print(functions.RTN())
 
 for i, email in enumerate(employees.EMPLOYEES, 1):
     NUMS.append(i)
     EMP_DCT[i] = email
 
 # prompt user to enter start employee's pto start and end dates
-print(f'Please select an employee.')
+print("\nPlease select an employee.")
 for num, email in EMP_DCT.items():
     print(num, email)
 
@@ -27,13 +27,13 @@ while True:
     print(functions.RTN())
     email = functions.switch_case(user_choice, EMP_DCT)
     print(email)
-    pto_start = functions.prompt_user_for_pto_start_end('pto start date',
-                                                        'pto_start')
-    pto_end = functions.prompt_user_for_pto_start_end('pto end date', 'pto_end')
+    pto_start = functions.prompt_user_for_pto_start_end("pto start date",
+                                                        "pto_start")
+    pto_end = functions.prompt_user_for_pto_start_end("pto end date", "pto_end")
     print(functions.RTN())
     PTO[email] = [pto_start,pto_end]
     if user_choice not in NUMS:
-        print('not an option ')
+        print("not an option")
     else:
         break
 
@@ -41,12 +41,12 @@ for email, pto in PTO.items():
     pto_start = pto[0]
     pto_end = pto[1]
 
-print(f'{email} will be taking pto from {pto_start} to {pto_end}')
+print(f"{email} will be taking pto from {pto_start} to {pto_end}")
 
-print(functions.RTN())
+# print(functions.RTN())
 
-print('booked pto')
-with open('pto.csv') as csv_file:
+print("\nbooked pto")
+with open("pto.csv") as csv_file:
     F_CSV = csv.reader(csv_file)
     COLUMN_HEADINGS = next(F_CSV)
     CSV_ROW = namedtuple('Row', COLUMN_HEADINGS)
@@ -58,15 +58,15 @@ with open('pto.csv') as csv_file:
 
 print(functions.RTN())
 
-pto_start_fmt = functions.fmt_date('pto_start_fmt', pto_start)
+pto_start_fmt = functions.fmt_date("pto_start_fmt", pto_start)
 BOOKED_PTO[pto_start_fmt] = email
 
-HEADERS = 'shift','email'
+# HEADERS = 'shift','email'
 
 print('updated pto')
 with open('pto.csv', 'w') as out_file:
     out_csv = csv.writer(out_file)
-    out_csv.writerow(HEADERS)
+    out_csv.writerow(["shift","email"])
     for shift, email in sorted(BOOKED_PTO.items()):
         keys_values = (shift, email)
         out_csv.writerow(keys_values)
